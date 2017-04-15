bce2c7eff408f9136d5ceaf90d02a42cd63fac03
José Corcuera <jzcorcuera@gmail.com>
2017-04-15 11:56:13 -0500

    Complete assigment for week 2.

--- week_2/programming_challenge/index.erl
+++ week_2/programming_challenge/index.erl
@@ -13,7 +13,10 @@
 get_file_contents(Name) ->
   {ok,File} = file:open(Name,[read]),
   Rev = get_all_lines(File,[]),
-  lists:reverse(Rev).
+  Lines = lists:reverse(Rev),
+  UnorderedResult = parse_lines(Lines),
+  Result = lists:sort(UnorderedResult),
+  file:write_file(string:join(["output-", Name], ""), io_lib:fwrite("~p.\n", [Result])).
 
 % Auxiliary function for get_file_contents.
 % Not exported.
@@ -38,4 +41,43 @@ show_file_contents([L|Ls]) ->
 show_file_contents([]) ->
   ok.
 
+parse_lines(Lines) ->
+  parse_lines(Lines, 1, []).
 
+parse_lines([], _LineNumber, Dictionary) ->
+  Dictionary;
+
+parse_lines([L|Ls], LineNumber, Dictionary) when length(L) =:= 0 ->
+  parse_lines(Ls, LineNumber + 1, Dictionary);
+
+parse_lines([L|Ls], LineNumber, Dictionary) ->
+  case re:run(L, "[A-Za-z]{4,}", [global, {capture, all, list}]) of
+    {match, Words} ->
+      NewDictionary = update_dictionary(Words, LineNumber, Dictionary),
+      parse_lines(Ls, LineNumber + 1, NewDictionary);
+    _ ->
+      parse_lines(Ls, LineNumber + 1, Dictionary)
+  end.
+
+update_dictionary([], _LineNumber, Dictionary) ->
+  Dictionary;
+
+update_dictionary([[W]|Ws], LineNumber, Dictionary) ->
+  NormalizedWord = string:to_lower(W),
+  case lists:keytake(NormalizedWord, 1, Dictionary) of
+    {value, {NormalizedWord, WordIndexes}, NewDictionary} ->
+      NewIndexes = add_index(LineNumber, WordIndexes),
+      update_dictionary(Ws, LineNumber, [{NormalizedWord, NewIndexes} | NewDictionary]);
+    _ ->
+      update_dictionary(Ws, LineNumber, [{NormalizedWord, [{LineNumber, LineNumber}]} | Dictionary])
+  end.
+
+add_index(LineNumber, Indexes)->
+  {First, Last} = lists:last(Indexes),
+  case LineNumber =:= Last orelse LineNumber =:= Last + 1 of
+    true ->
+      NewIndexes = lists:droplast(Indexes),
+      NewIndexes ++ [{First, LineNumber}];
+    _ ->
+      Indexes ++ [{LineNumber, LineNumber}]
+  end.
